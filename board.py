81c6789d5030e6fb9df46a59eca2ef966fdd8711
J0seph09 <meirelesjoseph@gmail.com>
2024-12-05 19:31:07 -0500

    Update board.py

--- board.py
+++ board.py
@@ -20,8 +20,8 @@ class Board:
         restart_screen = restart.render('Restart', False, (0, 0, 0))
         self.screen.blit(restart_screen, (250, 620))
 
-        exit = pygame.font.SysFont('Arial', 30)
-        exit_screen = exit.render('Exit', False, (0, 0, 0))
+
+        exit_screen = reset.render('Exit', False, (0, 0, 0))
         self.screen.blit(exit_screen, (475, 620))
 
         # horizontal lines
@@ -32,7 +32,7 @@ class Board:
             pygame.draw.line(self.screen, (0, 0, 0), (i * 200, 0), (i * 200, 600), 4)
         # smaller horizontal lines
         for i in range(1, 9):
-            pygame.draw.line(self.screen, (0, 0, 0), (0, i * 600 / 9), (600, i * 600 / 9, 600), 2)
+            pygame.draw.line(self.screen, (0, 0, 0), (0, i * 600 / 9), (600, i * 600 / 9), 2)
         # smaller vertical lines
         for i in range(1, 9):
             pygame.draw.line(self.screen, (0, 0, 0), (i * 600 / 9, 0), (i * 600 / 9, 600), 2)
@@ -46,9 +46,9 @@ class Board:
 
     def click(self, row, col):
         mousex, mousey = row, col
-        if mousex in range(0, 600) and mousey in range(0, 600):
-            row = mousey // 600
-            col = mousex // 600
+        if 0 <= mousex < self.width and 0 <= mousey < self.height:
+            row = int(mousey // ( self.height/9))
+            col = int(mousex // (self.width/9))
             self.selected = (row, col)
             return self.selected
         else:
@@ -57,14 +57,19 @@ class Board:
     def clear(self):
         if self.selected:
             row, col = self.selected
-            if self.original_board[row][col] == 0:
-                self.board[row][col] = 0
+            if 0 <= row < len(self.board) and 0 <= col < len(self.board[0]):
+                if self.board[row][col] == 0:
+                    self.board[row][col] = 0
     def sketch(self, value):
-        xval, yval = self.click
-        font = pygame.font.Font(None, 10)
-        text = font.render(value, True, (211, 211, 211))
-        screen.blit(text, ((xval*200) + 5, (yval*200) + 5)
-        pygame.display.update()
+       if self.selected:
+           row, col = self.selected
+           if 0 <= row < len(self.board) and 0 <= col < len(self.board[0]):
+               font = pygame.font.Font(None, 20)
+               text = font.render(str(value), True, (211, 211, 211))
+               xval = col (600/9) +5
+               yval = row (600/9) +5
+               self.screen.blit(text, xval, yval)
+               pygame.display.update()
 
     def place_number(self, value):
         if self.selected:
@@ -73,43 +78,48 @@ class Board:
                 self.board[row][col] = value
                 self.draw()
                 font = pygame.font.SysFont('Arial', 40)
-                text = font.render(value, True, (255, 255, 255))
-                x = col * 50 + 25 - text.get_width() / 2
-                y = row * 50 + 25 - text.get_height() / 2
+                text = font.render(str(value), True, (255, 255, 255))
+                x = col * (600 / 9) + ((600 / 9) / 2) - (text.get_width() / 2)
+                y = row * (600 / 9) + ((600 / 9) / 2) - (text.get_height() / 2)
                 self.screen.blit(text, (x, y))
                 pygame.display.update()
     def reset_to_original(self):
-        self.board = [row[:] for row in self.original_board]
+        if self.original_board:
+            self.board = [row[:] for row in self.original_board]
     def is_full(self):
-        test = SudokuGenerator.get_board()
-        for row in test:
-            for col in test:
-                if test[row][col] == 0: return False
+        for row in self.board:
+            if 0 in row:
+                return False
         return True
+
     def update_board(self):
-               for row in range(9):
-            for col in range(9):
-                self.original_board[row][col] = self.board[row][col]
+        if self.original_board and self.board:
+            for row in range(len(self.board)):
+                for col in range(len(self.board[0])):
+                    self.original_board [row][col] = self.board[row][col]
     def find_empty(self):
          for row in range(9):
             for col in range(9):
-                if self.board[row][col] == 0:  
+                if self.board[row][col] == 0:
                     return row, col
     def check_board(self):
-         for row in range(9):
+         for row in range(len(self.board)):
             value = set()
             for num in self.board[row]:
                 if num != 0:
                     if num in value or not (1 <= num <= 9):
                         return False
-            for col in range(9):
+                    value.add(num)
+            for col in range(len(self.board[0])):
                 value = set()
-                for num in self.board[col]:
+                for row in range(len(self.board)):
+                    num = self.board[row][col]
                     if num != 0:
                         if num in value or not (1 <= num <= 9):
                             return False
-            for start_row in range(0,3,9):
-                for start_col in range(0,3,9):
+                        value.add(num)
+            for start_row in range(0,len(self.board), 3):
+                for start_col in range(0, len(self.board[0]), 3):
                     value = set()
                     for row in range(3):
                         for col in range(3):
@@ -117,4 +127,5 @@ class Board:
                             if num != 0:
                                 if num in value or not (1 <= num <= 9):
                                     return False
+                                value.add(num)
             return True

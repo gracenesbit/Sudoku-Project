cfff876054fbe486913ed9888d21c2ab1447ad92
J0seph09 <meirelesjoseph@gmail.com>
2024-12-06 14:13:20 -0500

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,5 +1,4 @@
-import pygame, sys
-pygame.init()
+import pygame
 
 class Cell:
     def __init__(self, value, row, col, screen):
@@ -7,12 +6,31 @@ class Cell:
         self.row = row
         self.col = col
         self.screen = screen
-    def find_dimensions(self, row, col):
-        x_exact = (col * (600/9)) + (600/18) - 9
-        y_exact = (row * (600/9)) + (600/18) - 20
-        return x_exact, y_exact
-    def draw(self, color):
-        number = pygame.font.SysFont('Arial', 40)
-        place_num = number.render(str(self.value), False, color)
-        x, y = self.find_dimensions(self.row, self.col)
-        self.screen.blit(place_num, (x, y))
+        self.selected = False
+        self.sketched_value = 0
+
+    def set_cell_value(self, value):
+        self.value = value
+
+    def set_sketched_value(self, value):
+        self.sketched_value = value
+
+    def draw(self):
+        cell_width = 600 / 9
+        cell_height = 600 / 9
+        x = self.col * cell_width
+        y = self.row * cell_height
+
+        if self.selected:
+            pygame.draw.rect(self.screen, (255, 0, 0), (x, y, cell_width, cell_height), 3)
+
+        if self.value != 0:
+            font = pygame.font.SysFont("arial", 40)
+            text = font.render(str(self.value), True, (0, 0, 0))
+            text_rect = text.get_rect(center=(x + cell_width / 2, y + cell_height / 2))
+            self.screen.blit(text, text_rect)
+
+        elif self.sketched_value != 0:
+            font = pygame.font.SysFont("arial", 20)
+            text = font.render(str(self.sketched_value), True, (128, 128, 128))
+            self.screen.blit(text, (x + 5, y + 5))
